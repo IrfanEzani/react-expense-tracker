ced91553978813a25aef4a4587156f50362dd4fa
IrfanEzani <irfanezani@gmail.com>
2020-07-09 15:32:50 +0800

    first commit

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <div>
+             <h4>Your Balance</h4> 
+            <p id="balance">RM0.00</p>
+        </div>
+    )
+}
